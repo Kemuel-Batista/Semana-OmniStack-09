b0d0f5c4dece795552562dcf76246822068e2649
Kemuel Batista <Kemuellima20@gmail.com>
2020-05-16 20:33:20 -0300

    Air CnC

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -2,12 +2,12 @@ const User = require('../models/User')
 
 module.exports = {
     async store(req, res){
-        const { email, username, password } = req.body;
+        const { email } = req.body;
 
         let user = await User.findOne({ email });
 
         if(!user){
-            user = await User.create({ email, username, password })
+            user = await User.create({ email })
         }
 
         return res.json(user);
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     email: String,
-    username: String,
-    password: Number
 })
 
 module.exports = mongoose.model('User', UserSchema);
