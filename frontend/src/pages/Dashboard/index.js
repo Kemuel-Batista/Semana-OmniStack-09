f127cd64ee28a6b0a2857231961589e0193598cd
Kemuel Batista <Kemuellima20@gmail.com>
2020-05-17 12:49:43 -0300

    Air CnC

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
+
+import './styles.css';
 
 export default function Dashboard(){
     const [spots, setSpots] = useState([]);
@@ -21,12 +24,16 @@ export default function Dashboard(){
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
-                        <header />
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}/>
                         <strong>{spot.company}</strong>
-                        <span>{spot.price}</span>
+                        <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
                     </li>
                 ))}
             </ul>
+
+            <Link to="/new">
+                Cadastrar novo spot
+            </Link>
         </>
     );
 }
